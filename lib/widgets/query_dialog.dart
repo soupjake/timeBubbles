2696d75ee030115ba0c077b645fb60daf8bbfda6
Jake Gough <super.jake@hotmail.co.uk>
2018-11-15 11:55:59 +0000

    Update query_dialog.dart

--- lib/widgets/query_dialog.dart
+++ lib/widgets/query_dialog.dart
@@ -12,6 +12,7 @@ class QueryDialog extends StatefulWidget {
 }
 
 class QueryDialogState extends State<QueryDialog> {
+  List<String> original;
   TextField ingredientField;
   TextEditingController ingredientController = new TextEditingController();
   bool vegan = false;
@@ -21,10 +22,11 @@ class QueryDialogState extends State<QueryDialog> {
 
   @override
   void initState() {
+    original = new List<String>.from(widget.ingredients);
     ingredientField = new TextField(
         controller: ingredientController,
         decoration: InputDecoration(hintText: "Add ingredient..."));
-    for (int i = widget.ingredients.length-1; i > 0; i--) {
+    for (int i = widget.ingredients.length - 1; i > 0; i--) {
       switch (widget.ingredients[i]) {
         case "vegan":
           vegan = true;
@@ -58,96 +60,94 @@ class QueryDialogState extends State<QueryDialog> {
     return SimpleDialog(
       contentPadding: EdgeInsets.all(12.0),
       children: <Widget>[
-        Padding(
-            padding: EdgeInsets.only(
-                top: 8.0, bottom: 16.0, left: 12.0, right: 12.0),
-            child: Row(
-              children: <Widget>[
-                Expanded(
-                    child: Theme(
-                        data: ThemeData(primaryColor: Colors.black54),
-                        child: ingredientField)),
-                IconButton(
-                  icon: Icon(Icons.add),
-                  onPressed: () {
-                    setState(() {
-                      if (ingredientController.text.length > 0) {
-                        widget.ingredients.add(ingredientController.text);
-                      }
-                    });
-                  },
-                )
-              ],
-            )),
-        Row(children: <Widget>[
-          ChipsTile(
-            ingredients: widget.ingredients,
-            deletable: true,
-          )
-        ]),
-        Row(mainAxisAlignment: MainAxisAlignment.spaceAround,
+        Row(
           children: <Widget>[
-          Text("Vegan      "),
-          Checkbox(
-            value: vegan,
-            onChanged: (value) {
-              setState(() {
-                vegan = value;
-              });
-            },
-          ),
-          Text("Vegetarian"),
-          Checkbox(
-            value: vegetarian,
-            onChanged: (value) {
-              setState(() {
-                vegetarian = value;
-              });
-            },
-          ),
-        ]),
-        Row(mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: <Widget>[
-          Text("Diary Free"),
-          Checkbox(
-            value: diaryFree,
-            onChanged: (value) {
-              setState(() {
-                diaryFree = value;
-              });
-            },
-          ),
-          Text("Gluten Free"),
-          Checkbox(
-            value: glutenFree,
-            onChanged: (value) {
-              setState(() {
-                glutenFree = value;
-              });
-            },
-          ),
-        ]),
+            Expanded(
+                child: Theme(
+                    data: ThemeData(primaryColor: Colors.black54),
+                    child: ingredientField)),
+            IconButton(
+              icon: Icon(Icons.add),
+              onPressed: () {
+                setState(() {
+                  if (ingredientController.text.length > 0) {
+                    widget.ingredients.add(ingredientController.text);
+                    ingredientController.clear();
+                  }
+                });
+              },
+            )
+          ],
+        ),
+        ChipsTile(
+          ingredients: widget.ingredients,
+          deletable: true,
+        ),
+        Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Text("Vegan      "),
+              Checkbox(
+                value: vegan,
+                onChanged: (value) {
+                  setState(() {
+                    vegan = value;
+                  });
+                },
+              ),
+              Text("Vegetarian"),
+              Checkbox(
+                value: vegetarian,
+                onChanged: (value) {
+                  setState(() {
+                    vegetarian = value;
+                  });
+                },
+              ),
+            ]),
+        Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Text("Diary Free"),
+              Checkbox(
+                value: diaryFree,
+                onChanged: (value) {
+                  setState(() {
+                    diaryFree = value;
+                  });
+                },
+              ),
+              Text("Gluten Free"),
+              Checkbox(
+                value: glutenFree,
+                onChanged: (value) {
+                  setState(() {
+                    glutenFree = value;
+                  });
+                },
+              ),
+            ]),
         Row(
           mainAxisAlignment: MainAxisAlignment.end,
           children: <Widget>[
             FlatButton(
                 child: Text("Cancel"),
                 onPressed: () async {
-                  Navigator.of(context).pop();
+                  Navigator.of(context).pop(original);
                 }),
             FlatButton(
                 child: Text("Accept"),
                 onPressed: () async {
-                  if(vegan){
+                  if (vegan) {
                     widget.ingredients.add("vegan");
                   }
-                  if(vegetarian){
+                  if (vegetarian) {
                     widget.ingredients.add("vegetarian");
                   }
-                  if(diaryFree){
+                  if (diaryFree) {
                     widget.ingredients.add("diaryFree");
                   }
-                  if(glutenFree){
+                  if (glutenFree) {
                     widget.ingredients.add("glutenFree");
                   }
                   Navigator.of(context).pop(widget.ingredients);
