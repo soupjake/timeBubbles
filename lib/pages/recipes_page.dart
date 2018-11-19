24384b5f6415333dd3a36c8a7d940b92fe3a268d
Jake Gough <super.jake@hotmail.co.uk>
2018-11-19 09:52:11 +0000

    Update recipes_page.dart

--- lib/pages/recipes_page.dart
+++ lib/pages/recipes_page.dart
@@ -30,77 +30,81 @@ class RecipesPageState extends State<RecipesPage> {
   @override
   Widget build(BuildContext context) {
     return Material(
+        key: PageStorageKey<String>("RecipesKey"),
         child: Column(children: <Widget>[
-      Padding(
-          padding: const EdgeInsets.only(top: 8.0),
-          child: Row(crossAxisAlignment: CrossAxisAlignment.start, children: <
-              Widget>[
-            PopupMenuButton(
-              icon: Icon(Icons.sort),
-              itemBuilder: (context) => <PopupMenuItem>[
-                    new PopupMenuItem<int>(child: Text("Time"), value: 0),
-                    new PopupMenuItem<int>(child: Text("Servings"), value: 1),
-                    new PopupMenuItem<int>(child: Text("Score"), value: 2),
-                  ],
-              onSelected: (result) async {
-                switch (result) {
-                  case 0:
-                    setState(() {
-                      RecipeViewModel.recipes.sort((a, b) =>
-                          a.readyInMinutes.compareTo(b.readyInMinutes));
-                    });
-                    break;
-                  case 1:
-                    setState(() {
-                      RecipeViewModel.recipes
-                          .sort((a, b) => b.servings.compareTo(a.servings));
-                    });
-                    break;
-                  case 2:
-                    setState(() {
-                      RecipeViewModel.recipes.sort((a, b) =>
-                          b.spoonacularScore.compareTo(a.spoonacularScore));
-                    });
-                    break;
-                }
-              },
-            ),
-            Expanded(
-                child: Container(
-              decoration: const BoxDecoration(
-                  border: Border(
-                      bottom: BorderSide(width: 1.0, color: Colors.black54))),
-              child: ChipsTile(
-                ingredients: ingredients,
-                deletable: false,
-              ),
-            )),
-            IconButton(
-                icon: Icon(Icons.edit),
-                onPressed: () async {
-                  List<String> temp = await showDialog(
-                      context: context,
-                      builder: (context) {
-                        return QueryDialog(ingredients: ingredients);
+          Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child:
+                  Row(crossAxisAlignment: CrossAxisAlignment.start, children: <
+                      Widget>[
+                PopupMenuButton(
+                  icon: Icon(Icons.sort),
+                  itemBuilder: (context) => <PopupMenuItem>[
+                        PopupMenuItem<int>(child: Text("Time"), value: 0),
+                        PopupMenuItem<int>(child: Text("Servings"), value: 1),
+                        PopupMenuItem<int>(child: Text("Score"), value: 2),
+                      ],
+                  onSelected: (result) async {
+                    switch (result) {
+                      case 0:
+                        setState(() {
+                          RecipeViewModel.recipes.sort((a, b) =>
+                              a.readyInMinutes.compareTo(b.readyInMinutes));
+                        });
+                        break;
+                      case 1:
+                        setState(() {
+                          RecipeViewModel.recipes
+                              .sort((a, b) => b.servings.compareTo(a.servings));
+                        });
+                        break;
+                      case 2:
+                        setState(() {
+                          RecipeViewModel.recipes.sort((a, b) =>
+                              b.spoonacularScore.compareTo(a.spoonacularScore));
+                        });
+                        break;
+                    }
+                  },
+                ),
+                Expanded(
+                    child: Container(
+                  decoration: const BoxDecoration(
+                      border: Border(
+                          bottom:
+                              BorderSide(width: 1.0, color: Colors.black54))),
+                  child: ChipsTile(
+                    ingredients: ingredients,
+                    deletable: false,
+                  ),
+                )),
+                IconButton(
+                    icon: Icon(Icons.edit),
+                    onPressed: () async {
+                      List<String> temp = await showDialog(
+                          context: context,
+                          builder: (context) {
+                            return QueryDialog(ingredients: ingredients);
+                          });
+                      setState(() {
+                        ingredients = temp;
+                        //do api stuff here
                       });
-                  setState(() {
-                    ingredients = temp;
+                    }),
+              ])),
+          Flexible(child: PageTransformer(
+            pageViewBuilder: (context, visibilityResolver) {
+              return PageView.builder(
+                  controller: PageController(viewportFraction: 0.75),
+                  itemCount: RecipeViewModel.recipes.length,
+                  itemBuilder: (context, index) {
+                    return PageItem(
+                        recipe: RecipeViewModel.recipes[index],
+                        pageVisibility:
+                            visibilityResolver.resolvePageVisibility(index));
                   });
-                }),
-          ])),
-      Flexible(child: PageTransformer(
-        pageViewBuilder: (context, visibilityResolver) {
-          return PageView.builder(
-              controller: PageController(viewportFraction: 0.75),
-              itemCount: RecipeViewModel.recipes.length,
-              itemBuilder: (context, index) {
-                return PageItem(
-                    recipe: RecipeViewModel.recipes[index],
-                    pageVisibility:
-                        visibilityResolver.resolvePageVisibility(index));
-              });
-        },
-      ))
-    ]));
+            },
+          ))
+        ]));
   }
 }
